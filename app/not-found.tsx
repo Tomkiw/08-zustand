219df4c30adb09494c0296a9a2b145e360de5a9a
Tomkiv_Ihor <igortomkiw3@gmail.com>
2026-04-10 12:02:03 +0300

    Add Zustand for state management and update global styles with Roboto font

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,23 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "404 - Page not found METADATA",
+  description: "Sorry, the page you are looking for does not exist. METADATA",
+  openGraph: {
+    title: "404 - Page not found OG",
+    description: "Sorry, the page you are looking for does not exist. OG",
+    url: "https://notehub.com",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg?_gl=1*qwbrw9*_ga*MTE2Mjc1MTUxLjE3NzU0ODYwMjM.*_ga_PW0T7S5LDQ*czE3NzU4MDkyOTIkbzE1JGcxJHQxNzc1ODA5MzEzJGozOSRsMCRoMA..",
+        width: 1200,
+        height: 630,
+        alt: "image notehub NOT FOUND ",
+      },
+    ],
+  },
+};
+
 const NotFound = () => {
   return (
     <>
